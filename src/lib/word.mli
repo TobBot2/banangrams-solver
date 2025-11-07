c9dcd9d71766888e7e8486be2024482f7d6ae73b
PrincessMalu <malavika363.nair@gmail.com>
2025-11-07 14:27:31 -0500

    banana-dist.txt + combine word + direction modules in word.mli

--- src/lib/word.mli
+++ src/lib/word.mli
@@ -1,36 +1,60 @@
 open Core
 
 (** Represents a word found on the board *)
+module Direction : sig
+  (** Direction for word placement and navigation *)
 
-type t = {
-  start : Tile.Position.t;
-  direction : Direction.t;
-  chars : char list;
-} [@@deriving sexp, compare, equal]
+  type t =
+    | Horizontal
+    | Vertical
+  [@@deriving sexp, compare, equal, enumerate]
 
-val create : Tile.Position.t -> Direction.t -> char list -> t
-(** Create a word *)
+  val opposite : t -> t
+  (** [opposite Horizontal] = Vertical and vice versa *)
 
-val start : t -> Tile.Position.t
-(** Starting position of the word *)
+  val to_string : t -> string
+  (** "Horizontal" or "Vertical" *)
 
-val direction : t -> Direction.t
-(** Direction of the word *)
+  val of_string : string -> t
+  (** Parse from string (case-insensitive, accepts "horizontal" / "vertical", etc.) *)
 
-val chars : t -> char list
-(** Characters in the word *)
+  include Comparable.S with type t := t
+  include Sexpable.S with type t := t
+end
 
-val to_string : t -> string
-(** Convert word to string (e.g., "HELLO") *)
+module Word : sig
 
-val length : t -> int
-(** Number of characters in word *)
+  type t = {
+    start : Tile.Position.t;
+    direction : Direction.t;
+    chars : char list;
+  } [@@deriving sexp, compare, equal]
 
-val positions : t -> Tile.Position.t list
-(** All positions on the board used up by this word *)
+  val create : Tile.Position.t -> Direction.t -> char list -> t
+  (** Create a word *)
 
-val contains_position : Tile.Position.t -> t -> bool
-(** Check if word is within given position *)
+  val start : t -> Tile.Position.t
+  (** Starting position of the word *)
 
-include Comparable.S with type t := t
-include Sexpable.S with type t := t
+  val direction : t -> Direction.t
+  (** Direction of the word *)
+
+  val chars : t -> char list
+  (** Characters in the word *)
+
+  val to_string : t -> string
+  (** Convert word to string (e.g., "HELLO") *)
+
+  val length : t -> int
+  (** Number of characters in word *)
+
+  val positions : t -> Tile.Position.t list
+  (** All positions on the board used up by this word *)
+
+  val contains_position : Tile.Position.t -> t -> bool
+  (** Check if word is within given position *)
+
+  include Comparable.S with type t := t
+  include Sexpable.S with type t := t
+
+end
